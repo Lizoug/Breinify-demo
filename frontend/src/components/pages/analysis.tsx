063e6945f953430f7ebebba738e42c1e42159d7e
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-10-30 20:10:18 +0100

    added xl

--- frontend/src/components/pages/analysis.tsx
+++ frontend/src/components/pages/analysis.tsx
@@ -9,7 +9,7 @@ import { useEffect } from "react";
 
 
 // Analysis is a React component for visualizing data based on different algorithms and parameters
-export default function Analysis() {
+export default function Analysis_xxl() {
     const [algorithm, setAlgorithm] = useState<string>("umap");
     const [n, setN] = useState<number>(2);
     const [days, setDays] = useState<[number, number]>([0, 6]);
@@ -65,66 +65,68 @@ export default function Analysis() {
 
 
     return (
-        <Row justify="space-evenly" className="container">
-            <Col span={12} className="A-main Box-Design">
+        <Row justify="space-evenly" className="container_l">
+            <Col span={11} className="A-main Box-Design_l">
                 <h2 className="section-title">Main Visualization</h2>
                 <VizScatter
                     data={mainVisualizationData}
-                    fsize={12}
+                    fsize={16}
                     algoName={algoName}
                     n_component={n}
                     includeToolbox={true}
                 />
 
             </Col>
-            <Col span={6} className="B-history Box-Design">
+            <Col span={7} className="Box-Design_l B-history_xl">
                 <h2 className="section-title">History</h2>
-                {[...history].reverse().map((entry, index) => (
-                    <div
-                        key={index}
-                        onClick={() => {
-                            setMainVisualizationData(entry.data);
-                            setAlgorithm(entry.algoName);
-                            setN(entry.n_component);
-                        }}
-                    >
-                        <VizScatter
-                            data={entry.data}
-                            fsize={5}
-                            algoName={entry.algoName}
-                            n_component={entry.n_component}
-                            includeToolbox={false}
-                        />
-                    </div>
-                ))}
+                <div className="history-wrapper_xl">
+                    {[...history].reverse().map((entry, index) => (
+                        <div
+                            key={index}
+                            onClick={() => {
+                                setMainVisualizationData(entry.data);
+                                setAlgorithm(entry.algoName);
+                                setN(entry.n_component);
+                            }}
+                        >
+                            <VizScatter
+                                data={entry.data}
+                                fsize={14}
+                                algoName={entry.algoName}
+                                n_component={entry.n_component}
+                                includeToolbox={false}
+                            />
+                        </div>
+                    ))}
+                </div>
 
             </Col>
-            <Col span={5} className="C-option Box-Design">
+            <Col span={5} className="C-option_l Box-Design_l">
                 <h2 className="section-title">Event Parameters</h2>
-                <Row className="input-row">
-                    <label className="input-label">Algorithm:</label>
+                <Row className="input-row_l">
+                    <label className="input-label_l">Algorithm:</label>
                     <select id="algorithm" value={algorithm} onChange={handleAlgorithmChange}
-                            className="custom-select">
+                            className="custom-select_l">
                         <option value="pca">PCA</option>
                         <option value="umap">UMAP</option>
                         <option value="tsne">t-SNE</option>
                     </select>
                 </Row>
 
-                <Row className="input-row">
-                    <label className="input-label">Number of Components:</label>
+                <Row className="input-row_l">
+                    <label className="input-label_l">Number of Components:</label>
                     <input
                         type="number"
                         id="n"
                         value={n}
                         onChange={handleNumberChange}
-                        className="custom-input"
+                        className="custom-input_l"
                     />
                 </Row>
                 <Row>
                     <label>Filter  </label>
                 </Row>
-                <Row className="space-below">
+                <Row className="space-below_l">
                     <Space direction="vertical">
                         <Switch
                             checked={filterToggle}
@@ -136,8 +138,8 @@ export default function Analysis() {
                     </Space>
                 </Row>
                 {filterToggle && <>
-                    <Row className="input-row">
-                        <label className="input-label">Hours:</label>
+                    <Row className="input-row_l">
+                        <label className="input-label_l">Hours:</label>
                     </Row>
                     <Row>
                         <Slider
@@ -146,12 +148,12 @@ export default function Analysis() {
                             max={24}
                             onChange={handleHoursChange}
                             value={hours}
-                            className="custom-input"
+                            className="custom-input_l"
                         />
                     </Row>
 
-                    <Row className="input-row">
-                        <label className="input-label">Days:</label>
+                    <Row className="input-row_l">
+                        <label className="input-label_l">Days:</label>
                     </Row>
                     <Row>
                         <Slider
@@ -160,12 +162,12 @@ export default function Analysis() {
                             max={6}
                             onChange={handleDaysChange}
                             value={days}
-                            className="custom-input"
+                            className="custom-input_l"
                         />
                     </Row>
 
-                    <Row className="input-row">
-                        <label className="input-label">Weeks:</label>
+                    <Row className="input-row_l">
+                        <label className="input-label_l">Weeks:</label>
                     </Row>
                     <Row>
                         <Slider
@@ -174,12 +176,12 @@ export default function Analysis() {
                             max={52}
                             onChange={handleWeeksChange}
                             value={weeks}
-                            className="custom-input"
+                            className="custom-input_l"
                         />
                     </Row>
 
-                    <Row className="input-row">
-                        <label className="input-label">Months:</label>
+                    <Row className="input-row_l">
+                        <label className="input-label_l">Months:</label>
                     </Row>
                     <Row>
                         <Slider
@@ -188,13 +190,13 @@ export default function Analysis() {
                             max={12}
                             onChange={handleMonthsChange}
                             value={months}
-                            className="custom-input"
+                            className="custom-input_l"
                         />
                     </Row>
                 </>
                 }
-                <Row className="input-row">
-                    <button onClick={handleFilterClick} className="custom-button">Visualize</button>
+                <Row className="input-row_l">
+                    <button onClick={handleFilterClick} className="custom-button_l">Visualize</button>
                 </Row>
 
             </Col>
--- frontend/src/components/pages/analysis_main.tsx
+++ frontend/src/components/pages/analysis_main.tsx
@@ -5,17 +5,21 @@ import Analysis_xs from "./analysis_xs";
 import Analysis_m from "./analysis_m";
 import Analysis_l from "./analysis_l";
 import Analysis_xl from "./analysis_xl";
+import Analysis_xxl from "./analysis";
+
 
 
 export default function Size() {
     const size_xxs = useMediaQuery({query: "(max-device-width: 479px)"});
     const size_xs = useMediaQuery({query: "(min-device-width: 480px) and (max-device-width: 768px)"});
     const size_m = useMediaQuery({query: "(min-device-width: 769px) and (max-device-width: 1024px"});
-    const size_l = useMediaQuery({query: "(min-device-width: 1025px) and (max-device-width: 1440px"});
+    const size_l = useMediaQuery({query: "(min-device-width: 1025px) and (max-device-width: 1599px)"});
+    const size_xl= useMediaQuery({query: "(min-device-width: 1600px) and (max-device-width: 1919px)"});
 
     if (size_xxs) return <Analysis_xxs />;
     if (size_xs) return <Analysis_xs />;
     if (size_m) return <Analysis_m />;
     if (size_l) return <Analysis_l />;
-    return <Analysis_xl />;
+    if (size_xl) return <Analysis_xl />
+    return <Analysis_xxl />;
 }
